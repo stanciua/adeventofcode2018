66ed475d534e72df5cbda83a0f3b4a054ae082c7
Andrei Stanciu <stanciua@localhost.localdomain>
2018-12-14 15:49:45 +0200

    Finished Part 1

--- day7/src/main/java/Day7.java
+++ day7/src/main/java/Day7.java
@@ -1,20 +1,20 @@
-import com.sun.source.util.Trees;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//Step C must be finished before step A can begin.
 class Day7 {
 
   private Map<Character, Set<Character>> steps = new HashMap<>();
   private Map<Character, Set<Character>> ancestors = new HashMap<>();
   private Set<Character> seenSteps = new TreeSet<>();
-  private final static Pattern pattern = Pattern
+  private static final Pattern pattern = Pattern
       .compile("Step ([A-Z]) must be finished before step ([A-Z]) can begin.");
 
   Day7() throws Exception {
@@ -34,72 +34,49 @@ class Day7 {
         }
       }
     }
-    System.out.println("steps: ");
-    steps.entrySet().stream().forEach(System.out::println);
-    for (char c : steps.values().stream().flatMap(s -> s.stream())
+    for (char c : steps.values().stream().flatMap(Set::stream)
         .collect(Collectors.toCollection(TreeSet::new))) {
       ancestors.put(c,
-          steps.entrySet().stream().filter(e -> e.getValue().contains(c)).map(e -> e.getKey())
+          steps.entrySet().stream().filter(e -> e.getValue().contains(c)).map(Map.Entry::getKey)
               .collect(Collectors.toCollection(TreeSet::new)));
     }
-    System.out.println("ancestors: ");
-    ancestors.entrySet().stream().forEach(System.out::println);
   }
 
-  private Optional<Character> getFirstStep() {
-    SortedSet<Character> parents = new TreeSet<>(steps.keySet());
-    parents.removeAll(steps.values().stream().flatMap(v -> v.stream())
+  private Set<Character> getStartingSteps() {
+    Set<Character> parents = new TreeSet<>(steps.keySet());
+    parents.removeAll(steps.values().stream().flatMap(Set::stream)
         .collect(Collectors.toCollection(TreeSet::new)));
-    return parents.stream().findFirst();
+    return new TreeSet<>(parents);
   }
 
-  int getResult1() {
-    char firstStep = getFirstStep().get();
-    seenSteps.add(firstStep);
-    Set<Character> toProcessSteps = steps.get(firstStep);
-//    System.out.print(firstStep);
-    processSteps(toProcessSteps);
-    return -1;
+  String getResult1() {
+    Set<Character> startSteps = getStartingSteps();
+    StringBuilder output = new StringBuilder();
+    processSteps(output, startSteps);
+    return output.toString();
   }
 
-  void processSteps(Set<Character> toProcessSteps) {
-    System.out.println("toProcessSteps: ");
-    toProcessSteps.stream().forEach(e -> System.out.print(e + " "));
-    System.out.println();
-    if (toProcessSteps.size() == 0) {
+  private void processSteps(StringBuilder output, Set<Character> toBeProcessedSteps) {
+    if (toBeProcessedSteps.isEmpty()) {
       return;
     }
 
-    char nextStep = toProcessSteps.stream().findFirst().get();
-//    System.out.print(nextStep);
-    seenSteps.add(nextStep);
-    toProcessSteps.remove(nextStep);
-
-    Set<Character> nextSteps = steps.get(nextStep);
-    System.out.println("nextSteps: ");
-    nextSteps.stream().forEach(e -> System.out.print(e + " "));
-    System.out.println();
-    if (nextSteps != null) {
-      Set<Character> notYetCompletedSteps = new TreeSet<>();
-      for (char step : nextSteps) {
-        boolean allAncestorsProcessed =
-            ancestors.get(step).stream()
-                .allMatch(as -> seenSteps.contains(as));
-        if (!allAncestorsProcessed) {
-          notYetCompletedSteps.add(step);
-        }
+    for (char c : toBeProcessedSteps) {
+      Set<Character> ancestorForStep = ancestors.get(c);
+      if (ancestorForStep != null && !seenSteps.containsAll(ancestorForStep)) {
+        continue;
       }
-
-      System.out.println("notYetCompletedSteps: ");
-      notYetCompletedSteps.stream().forEach(e -> System.out.print(e + " "));
-      System.out.println();
-      nextSteps.removeAll(notYetCompletedSteps);
-      System.out.println("nextSteps after filtering: ");
-      nextSteps.stream().forEach(e -> System.out.print(e + " "));
-      System.out.println();
-      toProcessSteps.addAll(nextSteps);
+      seenSteps.add(c);
+      output.append(c);
+      Set<Character> children = steps.get(c);
+      if (children != null) {
+        toBeProcessedSteps.addAll(children);
+      }
+      Set<Character> copy = new TreeSet<>(toBeProcessedSteps);
+      copy.remove(c);
+      processSteps(output, copy);
+      return;
     }
-    processSteps(toProcessSteps);
   }
 
   int getResult2() {
--- day7/src/test/java/Day7Test.java
+++ day7/src/test/java/Day7Test.java
@@ -8,7 +8,7 @@ public class Day7Test {
     public void testDay6Result1() {
         try {
             Day7 day7 = new Day7();
-            assertEquals(3290, day7.getResult1());
+            assertEquals("BITRAQVSGUWKXYHMZPOCDLJNFE", day7.getResult1());
         } catch(Exception e) {
             assert(false);
         }
