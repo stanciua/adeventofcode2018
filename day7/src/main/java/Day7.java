15fa1531a3c3420fc971fb5c8895e6a29b306681
stanciua <andreistanciu.as@gmail.com>
2018-12-22 23:18:09 +0200

    Day 7 part 2 finished, still needs some refactoring.

--- day7/src/main/java/Day7.java
+++ day7/src/main/java/Day7.java
@@ -1,25 +1,23 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import org.jgrapht.alg.connectivity.BlockCutpointGraph;
 
 class Day7 {
 
-  private static Map<Character, Set<Character>> steps = new HashMap<>();
-  private static Map<Character, Set<Character>> ancestors = new HashMap<>();
-  private static Set<Character> seenSteps = new TreeSet<>();
-  private static final Pattern pattern = Pattern
-      .compile("Step ([A-Z]) must be finished before step ([A-Z]) can begin.");
+  private final Map<Character, Set<Character>> steps = new HashMap<>();
+  private final Map<Character, Set<Character>> ancestors = new HashMap<>();
+  private final Set<Character> seenSteps = new TreeSet<>();
+  private static final Pattern pattern =
+      Pattern.compile("Step ([A-Z]) must be finished before step ([A-Z]) can begin.");
   private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   Day7() throws Exception {
@@ -34,23 +32,36 @@ class Day7 {
         if (steps.containsKey(parentStep)) {
           steps.get(parentStep).add(childStep);
         } else {
-          steps
-              .put(parentStep, Stream.of(childStep).collect(Collectors.toCollection(TreeSet::new)));
+          steps.put(
+              parentStep, Stream.of(childStep).collect(Collectors.toCollection(TreeSet::new)));
         }
       }
     }
-    for (char c : steps.values().stream().flatMap(Set::stream)
-        .collect(Collectors.toCollection(TreeSet::new))) {
-      ancestors.put(c,
-          steps.entrySet().stream().filter(e -> e.getValue().contains(c)).map(Map.Entry::getKey)
+    for (char c :
+        steps
+            .values()
+            .stream()
+            .flatMap(Set::stream)
+            .collect(Collectors.toCollection(TreeSet::new))) {
+      ancestors.put(
+          c,
+          steps
+              .entrySet()
+              .stream()
+              .filter(e -> e.getValue().contains(c))
+              .map(Map.Entry::getKey)
               .collect(Collectors.toCollection(TreeSet::new)));
     }
   }
 
-  private static Set<Character> getStartingSteps() {
+  private Set<Character> getStartingSteps() {
     Set<Character> parents = new TreeSet<>(steps.keySet());
-    parents.removeAll(steps.values().stream().flatMap(Set::stream)
-        .collect(Collectors.toCollection(TreeSet::new)));
+    parents.removeAll(
+        steps
+            .values()
+            .stream()
+            .flatMap(Set::stream)
+            .collect(Collectors.toCollection(TreeSet::new)));
     return new TreeSet<>(parents);
   }
 
@@ -61,134 +72,117 @@ class Day7 {
     return output.toString();
   }
 
-  private void processSteps(StringBuilder output, Set<Character> toBeProcessedSteps) {
-    if (toBeProcessedSteps.isEmpty()) {
-      return;
-    }
-
-    for (char c : toBeProcessedSteps) {
-      Set<Character> ancestorForStep = ancestors.get(c);
-      if (ancestorForStep != null && !seenSteps.containsAll(ancestorForStep)) {
-        continue;
-      }
-      seenSteps.add(c);
-      output.append(c);
-      Set<Character> children = steps.get(c);
-      if (children != null) {
-        toBeProcessedSteps.addAll(children);
-      }
-      Set<Character> copy = new TreeSet<>(toBeProcessedSteps);
-      copy.remove(c);
-      processSteps(output, copy);
-      return;
-    }
-  }
-
-  int getResult2() {
-    final int BOUND = 26;
-    final int PRODUCERS = 1;
-    final int CONSUMERS = 2;
-    final int POISON_PILL = Integer.MAX_VALUE;
-    final int POISON_PILL_PER_PRODUCER = CONSUMERS / PRODUCERS;
-
-    BlockingQueue<Integer> queue = new LinkedBlockingDeque<>(BOUND);
-    Thread producer = new Thread(new StepsProducer(queue, POISON_PILL, POISON_PILL_PER_PRODUCER));
-    producer.start();
-    try {
-      producer.join();
-    } catch(InterruptedException e){
-      e.printStackTrace();
-    }
-
-    // start the worker threads
-    Thread[] workers = new Thread[CONSUMERS];
-    for (int i = 0; i < workers.length; i++) {
-      workers[i] = new Thread(new Worker(queue, POISON_PILL));
-      workers[i].start();
-    }
-
-    try {
-      for (int i = 0; i < workers.length; i++) {
-        workers[i].join();
-      }
-    } catch( InterruptedException e) {
-      e.printStackTrace();
-    }
-    return -1;
-  }
-
-  static class Worker implements Runnable {
-    private BlockingQueue<Integer> queue;
-    private final int poisonPill;
-    public Worker(BlockingQueue<Integer> queue, int poisonPill) {
-      this.queue = queue;
-      this.poisonPill = poisonPill;
-    }
-
-    public void run() {
-      try {
-        while(true) {
-          int step = queue.take();
-          if (step == poisonPill) {
-            return;
-          }
-          for (int i = 0; i < step; i++){
-            System.out.println(Thread.currentThread().getName() + " sub-step " + i + " done");
-          }
-          System.out.println(Thread.currentThread().getName() + " step: " + step);
-        }
-      }catch(InterruptedException e) {
-        Thread.currentThread().interrupt();
-      }
-    }
-  }
-  static class StepsProducer implements Runnable {
-    private BlockingQueue<Integer> stepsQueue;
-    private final int poisonPill;
-    private final int poisonPillPerProducer;
-
-    public StepsProducer(BlockingQueue<Integer> numbersQueue, int poisonPill, int poisonPillPerProducer) {
-      this.stepsQueue = numbersQueue;
-      this.poisonPill = poisonPill;
-      this.poisonPillPerProducer = poisonPillPerProducer;
-    }
-    public void run() {
-      try {
-        Set<Character> startSteps = getStartingSteps();
-        produceSteps(startSteps);
-      } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
-      }
-    }
-
-    private void produceSteps(Set<Character> toBeProcessedSteps) throws InterruptedException {
-      if (toBeProcessedSteps.isEmpty()) {
-        return;
-      }
-
-      for (char c : toBeProcessedSteps) {
+  private void processSteps(StringBuilder output, Set<Character> stepsToProcess) {
+    outer:
+    while (!stepsToProcess.isEmpty()) {
+      for (char c : stepsToProcess) {
         Set<Character> ancestorForStep = ancestors.get(c);
         if (ancestorForStep != null && !seenSteps.containsAll(ancestorForStep)) {
           continue;
         }
         seenSteps.add(c);
-        System.out.println("Produced: " + (ALPHABET.indexOf(c) + 1));
-        stepsQueue.put(ALPHABET.indexOf(c) + 1);
+        output.append(c);
         Set<Character> children = steps.get(c);
         if (children != null) {
-          toBeProcessedSteps.addAll(children);
+          stepsToProcess.addAll(children);
         }
-        Set<Character> copy = new TreeSet<>(toBeProcessedSteps);
-        copy.remove(c);
-        produceSteps(copy);
-
-        // fill the poison pill for each consumer to terminate
-        for (int i = 0; i<poisonPillPerProducer; i++) {
-          stepsQueue.put(poisonPill);
-        }
-        return;
+        stepsToProcess.remove(c);
+        continue outer;
       }
     }
   }
-}
 
+  private int processSteps2(Set<Character> stepsToProcess) {
+    Worker[] workers = IntStream.range(0, 5).mapToObj(i -> new Worker()).toArray(Worker[]::new);
+    Set<Character> completedSteps = new TreeSet<>();
+    int duration = 0;
+    while (!stepsToProcess.isEmpty()) {
+      Set<Character> inProgressSteps =
+          Arrays.stream(workers)
+              .filter(w -> !w.isIdle())
+              .map(w1 -> w1.step)
+              .collect(Collectors.toCollection(TreeSet::new));
+      Set<Character> nextSteps = new TreeSet<>(stepsToProcess);
+      nextSteps.removeAll(inProgressSteps);
+      Worker[] availableWorkers =
+          Arrays.stream(workers).filter(Worker::isIdle).toArray(Worker[]::new);
+      for (char step : nextSteps) {
+        Set<Character> ancestorForStep = ancestors.get(step);
+        if (ancestorForStep != null && !seenSteps.containsAll(ancestorForStep)) {
+          continue;
+        }
+
+        if (availableWorkers.length != 0) {
+          availableWorkers[0].setSteps(ALPHABET.indexOf(step) + 1 + 60, step);
+          availableWorkers = Arrays.stream(workers).filter(Worker::isIdle).toArray(Worker[]::new);
+        } else {
+          break;
+        }
+      }
+
+      for (Worker worker : workers) {
+        worker.execute();
+        if (worker.isDone()) {
+          completedSteps.add(worker.getStep());
+        }
+      }
+      duration++;
+
+      for (char c : completedSteps) {
+        Set<Character> children = steps.get(c);
+        seenSteps.add(c);
+        if (children != null) {
+          stepsToProcess.addAll(children);
+        }
+        stepsToProcess.remove(c);
+      }
+      completedSteps.clear();
+    }
+
+    return duration;
+  }
+
+  int getResult2() {
+    Set<Character> startSteps = getStartingSteps();
+    return processSteps2(startSteps);
+  }
+
+  static class Worker {
+    private int noSteps;
+    private char step;
+    private boolean done = false;
+
+    Worker() {
+      this.noSteps = 0;
+    }
+
+    boolean isIdle() {
+      return noSteps == 0;
+    }
+
+    void setSteps(int noSteps, char step) {
+      this.done = false;
+      this.noSteps = noSteps;
+      this.step = step;
+    }
+
+    char getStep() {
+      return this.step;
+    }
+
+    void execute() {
+      if (isIdle()) {
+        done = false;
+      } else {
+        noSteps--;
+        if (noSteps == 0) {
+          done = true;
+        }
+      }
+    }
+
+    boolean isDone() {
+      return done;
+    }
+  }
+}
--- day7/src/test/java/Day7Test.java
+++ day7/src/test/java/Day7Test.java
@@ -4,20 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 public class Day7Test {
 
-//    @Test
-//    public void testDay6Result1() {
-//        try {
-//            Day7 day7 = new Day7();
-//            assertEquals("BITRAQVSGUWKXYHMZPOCDLJNFE", day7.getResult1());
-//        } catch(Exception e) {
-//            assert(false);
-//        }
-//    }
+    @Test
+    public void testDay6Result1() {
+        try {
+            Day7 day7 = new Day7();
+            assertEquals("BITRAQVSGUWKXYHMZPOCDLJNFE", day7.getResult1());
+        } catch(Exception e) {
+            assert(false);
+        }
+    }
     @Test
     public void testDay6Result2() {
         try {
             Day7 day7 = new Day7();
-            assertEquals(45602, day7.getResult2());
+            assertEquals(869, day7.getResult2());
         } catch(Exception e) {
             assert(false);
         }
