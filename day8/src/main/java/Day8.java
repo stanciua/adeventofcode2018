ee84a3b7fca9e43b7d4175a2e765636540d41dcb
stanciua <andreistanciu.as@gmail.com>
2018-12-24 14:18:38 +0200

    Finished part 1 and part 2.

--- day8/src/main/java/Day8.java
+++ day8/src/main/java/Day8.java
@@ -1,89 +1,91 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Day8 {
-  int[] tree;
+  private int[] tree;
 
   Day8() throws Exception {
     tree =
-        Files.lines(Path.of("src/test/java/input.txt"))
+        Files.lines(Path.of("/Users/stanciua/prog/adeventofcode2018/day8/src/test/java/input.txt"))
             .flatMap(line -> Arrays.stream(line.split(" ")))
-            .mapToInt(s -> Integer.parseInt(s))
+            .mapToInt(Integer::parseInt)
             .toArray();
   }
 
-  Node buildTree(int[] tree) {
+  private Node buildTree(int[] tree) {
+    ArrayList<Node> parents = new ArrayList<>();
+    int i = 2;
     Node root = new Node(tree[0], tree[1]);
-    buildTreeImpl(
-        root,
-        IntStream.range(2, tree.length)
-            .map(i -> tree[i])
-            .boxed()
-            .collect(Collectors.toCollection(ArrayList::new)));
+    parents.add(root);
+    while (i != tree.length) {
+      if (tree[i] == 0) {
+        Node child = new Node(tree[i], tree[i + 1]);
+        Node parent = parents.get(parents.size() - 1);
+        child.setParent(parent);
+        parent.addChild(child);
+        child.setMetadata(Arrays.copyOfRange(tree, i + 2, i + tree[i + 1] + 2));
+        i += tree[i + 1];
+        i += 2;
+      } else {
+        Node parent = parents.get(parents.size() - 1);
+        if (parent.noOfChilds == parent.childs.size()) {
+          parent.setMetadata(Arrays.copyOfRange(tree, i, i + parent.noOfMetada));
+          i += parent.noOfMetada;
+          parents.remove(parent);
+        } else {
+          Node child = new Node(tree[i], tree[i + 1]);
+          child.setParent(parent);
+          parent.addChild(child);
+          parents.add(child);
+          i += 2;
+        }
+      }
+    }
+
     return root;
   }
 
-  void buildTreeImpl(Node parent, ArrayList<Integer> tree) {
-    if (tree.size() == 0) {
-      return;
-    }
-    if (tree.get(0) == 0) {
-      Node child = new Node(tree.get(0), tree.get(1));
-      child.setMetadata(IntStream.range(2, 2 + tree.get(1)).map(i -> tree.get(i)).toArray());
-      IntStream.range(0, 2 + tree.get(1)).forEach(i -> tree.remove(0));
-      parent.addChild(child);
-      child.setParent(parent);
-      if (parent.noOfChilds != parent.childs.size()) {
-        buildTreeImpl(parent, tree);
-      } else {
-        parent.setMetadata(IntStream.range(0, parent.noOfMetada).map(i -> tree.get(i)).toArray());
-        IntStream.range(0, parent.noOfMetada).forEach(i -> tree.remove(0));
-      }
-    } else {
-      if (parent.noOfChilds != parent.childs.size()) {
-        Node child = new Node(tree.get(0), tree.get(1));
-        parent.addChild(child);
-        child.setParent(parent);
-        tree.remove(0);
-        tree.remove(0);
-        if (child.noOfChilds != child.childs.size()) {
-          buildTreeImpl(child, tree);
-        }
-      }
-      parent.setMetadata(IntStream.range(0, parent.noOfMetada).map(i -> tree.get(i)).toArray());
-      IntStream.range(0, parent.noOfMetada).forEach(i -> tree.remove(0));
-      System.out.println(tree.size());
-      buildTreeImpl(parent, tree);
-    }
-  }
-
-  void sumMetadata(Node node, ArrayList<ArrayList<Integer>> metadataTotal) {
+  private void sumMetadata(Node node, ArrayList<ArrayList<Integer>> metadataTotal) {
     if (node.noOfChilds == 0) {
       return;
     }
 
     for (Node child : node.childs) {
-      System.out.println(child.metadata);
       metadataTotal.add(child.metadata);
       sumMetadata(child, metadataTotal);
     }
+  }
+
+  private int findRootTotalValue(int value, Node node) {
+    if (node.noOfChilds != 0) {
+      for (int i = 0; i < node.metadata.size(); i++) {
+        int m = node.metadata.get(i) - 1;
+        if (m < node.childs.size()) {
+          value = findRootTotalValue(value, node.childs.get(m));
+        }
+      }
+    } else {
+      value += node.metadata.stream().mapToInt(i -> i).sum();
+    }
+
+    return value;
   }
 
   int getResult1() {
     Node root = buildTree(this.tree);
     ArrayList<ArrayList<Integer>> metadataTotal = new ArrayList<>();
     metadataTotal.add(root.metadata);
-    System.out.println(root.metadata);
     sumMetadata(root, metadataTotal);
 
-    return metadataTotal.stream().flatMap(meta -> meta.stream()).mapToInt(i -> i.intValue()).sum();
+    return metadataTotal.stream().flatMap(ArrayList::stream).mapToInt(i -> i).sum();
   }
 
   int getResult2() {
-    return -1;
+    Node root = buildTree(this.tree);
+    return findRootTotalValue(0, root);
   }
 
   static class Node {
--- day8/src/test/java/Day8Test.java
+++ day8/src/test/java/Day8Test.java
@@ -8,7 +8,7 @@ public class Day8Test {
     public void testDay6Result1() {
         try {
             Day8 day8 = new Day8();
-            assertEquals(-1, day8.getResult1());
+            assertEquals( 43996, day8.getResult1());
         } catch(Exception e) {
             assert(false);
         }
@@ -17,7 +17,7 @@ public class Day8Test {
     public void testDay6Result2() {
         try {
             Day8 day8 = new Day8();
-            assertEquals(-1, day8.getResult2());
+            assertEquals(35189, day8.getResult2());
         } catch(Exception e) {
             assert(false);
         }
