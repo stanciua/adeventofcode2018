132d455f53b804a413e84a0a38572541075dbe08
Andrei Stanciu <andreistanciu.as@gmail.com>
2019-04-19 17:50:44 +0300

    Day 20 part 1, refined the map build, still not working correctly, some offsets are wrong

--- day20/src/main/java/Day20.java
+++ day20/src/main/java/Day20.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import org.javatuples.Pair;
 
 import java.nio.file.Files;
@@ -7,95 +9,125 @@ import java.util.List;
 import java.util.Stack;
 
 class Day20 {
+
   char[][] map;
-  int originX;
-  int originY;
+
   Day20() throws Exception {
     String input = Files.readString(Path.of("src/test/java/input.txt"));
-    map = new char[10][10];
-    for (int i =0; i < map.length; i++) {
-      for (int j = 0; j <map[0].length; j++) {
-        map[i][j] = '?';
-      }
-    }
-    originX = map.length / 2;
-    originY = map[0].length / 2;
-    map[originX][originY] = 'X';
-    
-    getPaths(input);
-    
-    updateMapWithWalls();
+    List<Pair<Integer, Integer>> positionsList = new ArrayList<>();
+    List<Character> directionList = new ArrayList<>();
+    Map<Pair<Integer, Integer>, Character> roomDoorMap = new HashMap<>();
+    getPaths(input, roomDoorMap);
+    map = buildMap(roomDoorMap);
     displayMap();
   }
-  
+
   void updateMapWithWalls() {
-    for (int i =0; i < map.length; i++) {
-      for (int j = 0; j <map[0].length; j++) {
-        if (map[i][j] == '?') {
-          map[i][j] = '#';
-        }
-      }
-    }
   }
-  Pair<Integer, Integer> getNextPosition(Pair<Integer, Integer> currentPosition, char direction) {
+
+  Pair<Integer, Integer> nextPosition(Pair<Integer, Integer> currentPosition, char direction,
+      Map<Pair<Integer, Integer>, Character> map) {
     Pair<Integer, Integer> nextPosition;
+    Pair<Integer, Integer> doorPosition;
     int x = currentPosition.getValue0();
     int y = currentPosition.getValue1();
     switch (direction) {
       case 'E':
+        doorPosition = new Pair<>(x, y + 1);
         nextPosition = new Pair<>(x, y + 2);
-        map[x][y+2] = '.';
-        map[x][y+1] = '|';
+        map.put(doorPosition, '|');
         break;
       case 'W':
+        doorPosition = new Pair<>(x, y - 1);
         nextPosition = new Pair<>(x, y - 2);
-        map[x][y-2] = '.';
-        map[x][y-1] = '|';
+        map.put(doorPosition, '|');
         break;
       case 'N':
+        doorPosition = new Pair<>(x - 1, y);
         nextPosition = new Pair<>(x - 2, y);
-        map[x-2][y] = '.';
-        map[x-1][y] = '-';
+        map.put(doorPosition, '-');
         break;
       case 'S':
+        doorPosition = new Pair<>(x + 1, y);
         nextPosition = new Pair<>(x + 2, y);
-        map[x+2][y] = '.';
-        map[x+1][y] = '-';
+        map.put(doorPosition, '-');
         break;
       default:
         throw new IllegalArgumentException("Invalid coordinate received");
     }
+    map.put(nextPosition, '.');
     return nextPosition;
   }
-  
+
   void displayMap() {
-    for (int i =0; i < map.length; i++) {
-      for (int j = 0; j <map[0].length; j++) {
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map[0].length; j++) {
         System.out.print(map[i][j]);
       }
       System.out.println();
     }
   }
-  
-  List<Pair<Integer, Integer>> getPaths(String input) {
+
+  char[][] buildMap(Map<Pair<Integer, Integer>, Character> map) {
+    // find out the sizes of the grid based on the positions in the map
+    int minX = Integer.MAX_VALUE;
+    int maxX = Integer.MIN_VALUE;
+    int minY = Integer.MAX_VALUE;
+    int maxY = Integer.MIN_VALUE;
+    for (Pair<Integer, Integer> position : map.keySet()) {
+      minX = Integer.min(minX, position.getValue0());
+      minY = Integer.min(minY, position.getValue1());
+      maxX = Integer.max(maxX, position.getValue0());
+      maxY = Integer.max(maxY, position.getValue1());
+    }
+
+    int xSize = Math.abs(minX) + Math.abs(maxX) + 1 + 2;
+    int ySize = Math.abs(minY) + Math.abs(maxY) + 1 + 2;
+
+    char[][] grid = new char[xSize][ySize];
+    // first initialize the grid to be all ?
+    for (int i = 0; i < xSize; i++) {
+      for (int j = 0; j < ySize; j++) {
+        grid[i][j] = '?';
+      }
+    }
+    int originX = xSize / 2 + 1;
+    int originY = ySize / 2 + 1;
+    grid[originX][originY] = 'X';
+    // translate every position in the list by originX/originY and plot them in the grid,
+    // based on direction we will plot first the door (- or |) and right after the room (.)
+    for (Pair<Integer, Integer> position : map.keySet()) {
+      int x = position.getValue0();
+      int y = position.getValue1();
+      grid[originX + x][originY + y] = map.get(position);
+    }
+    // after we've done plotting all the positions, change all unknowns (?) with walls (#)
+    for (int i = 0; i < xSize; i++) {
+      for (int j = 0; j < ySize; j++) {
+        if (grid[i][j] == '?') {
+          grid[i][j] = '#';
+        }
+      }
+    }
+    return grid;
+  }
+
+  void getPaths(String input, Map<Pair<Integer, Integer>, Character> map) {
     Stack<Pair<Integer, Integer>> stack = new Stack<>();
-    List<Pair<Integer, Integer>> map = new ArrayList<>();
-    Pair<Integer, Integer> currentPosition = new Pair<>(originX, originY);
+    Pair<Integer, Integer> currentPosition = new Pair<>(0, 0);
     Pair<Integer, Integer> nextPosition;
     for (char c : input.toCharArray()) {
       if (c == 'E' || c == 'W' || c == 'N' || c == 'S') {
-        nextPosition = getNextPosition(currentPosition, c);
-        map.add(nextPosition);
+        nextPosition = nextPosition(currentPosition, c, map);
         currentPosition = nextPosition;
       } else if (c == '(') {
-        stack.push(map.get(map.size() - 1));
+        stack.push(currentPosition);
       } else if (c == '|') {
         currentPosition = stack.peek();
       } else if (c == ')') {
         currentPosition = stack.pop();
       }
     }
-    return map;
   }
 
   int getResult1() {
