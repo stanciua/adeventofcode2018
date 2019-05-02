075347a7a859e402555bca13ba8a2f5c7f895950
stanciua <andreistanciu.as@gmail.com>
2019-05-02 16:14:36 +0300

    Day 22 part 1 finised

---
+++ day22/src/main/java/Day22.java
@@ -0,0 +1,193 @@
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Day22 {
+  static final int DEPTH = 510;
+  static final int TARGETY = 10;
+  static final int TARGETX = 10;
+  static final int MOUTHY = 0;
+  static final int MOUTHX = 0;
+  static final int SIZEY = 16;
+  static final int SIZEX = 16;
+
+  List<Region> regions;
+  char[][] map;
+
+  Day22() throws Exception {
+    regions = new ArrayList<>();
+    initRegions();
+  }
+
+  void initRegions() {
+    // special cases for MOUTH
+    Region mouth = new Region(MOUTHY, MOUTHX);
+    mouth.calculateGeologicIndex(regions);
+    mouth.calculateErosionLevel(MOUTHY, MOUTHX);
+    mouth.setRegionType();
+    regions.add(mouth);
+    // special cases for TARGET
+    Region target = new Region(TARGETY, TARGETX);
+    target.calculateGeologicIndex(regions);
+    target.calculateErosionLevel(MOUTHY, MOUTHX);
+    target.setRegionType();
+    regions.add(target);
+    // special case for Y == 0
+    int y = 0;
+    for (int x = MOUTHX + 1; x < SIZEX; x++) {
+      Region region = new Region(y, x);
+      region.calculateGeologicIndex(regions);
+      region.calculateErosionLevel(y, x);
+      region.setRegionType();
+      regions.add(region);
+    }
+    // special case for X == 0
+    int x = 0;
+    for (y = MOUTHY + 1; y < SIZEY; y++) {
+      Region region = new Region(y, x);
+      region.calculateGeologicIndex(regions);
+      region.calculateErosionLevel(y, x);
+      region.setRegionType();
+      regions.add(region);
+    }
+    // the rest of the regions
+    for (y = MOUTHY + 1; y < SIZEY; y++) {
+      for (x = MOUTHX + 1; x < SIZEX; x++) {
+        if (y == TARGETY && x == TARGETX) {
+          continue;
+        }
+        Region region = new Region(y, x);
+        region.calculateGeologicIndex(regions);
+        region.calculateErosionLevel(y, x);
+        region.setRegionType();
+        regions.add(region);
+      }
+    }
+    regions.sort(Comparator.comparing(Region::getY).thenComparing(Region::getX));
+  }
+
+  char[][] buildMap() {
+    char[][] map = new char[SIZEY][SIZEX];
+    for (Region region : regions) {
+      if (region.y == MOUTHY && region.x == MOUTHX) {
+        map[region.y][region.x] = 'M';
+      } else if (region.y == TARGETY && region.x == TARGETX) {
+        map[region.y][region.x] = 'T';
+      } else if (region.regionType == RegionType.ROCKY) {
+        map[region.y][region.x] = '.';
+      } else if (region.regionType == RegionType.WET) {
+        map[region.y][region.x] = '=';
+      } else if (region.regionType == RegionType.NARROW) {
+        map[region.y][region.x] = '|';
+      }
+    }
+    return map;
+  }
+
+  int getResult1() {
+    int sum = 0;
+    List<Region> countedRegions =
+        this.regions.stream()
+            .filter(r -> r.y >= MOUTHY && r.x >= MOUTHX && r.y <= TARGETY && r.x <= TARGETX)
+            .collect(Collectors.toCollection(ArrayList::new));
+    
+    for (Region region: countedRegions) {
+      switch (region.regionType) {
+        case ROCKY:
+          sum += 0;
+          break;
+        case WET:
+          sum += 1;
+          break;
+        case NARROW:
+          sum += 2;
+          break;
+      }
+    }
+    return sum;
+  }
+
+  int getResult2() {
+    return -1;
+  }
+
+  static class Region {
+    int y;
+
+    public int getY() {
+      return y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public Region(int y, int x) {
+      this.y = y;
+      this.x = x;
+    }
+
+    @Override
+    public String toString() {
+      return "Region{" +
+              "y=" + y +
+              ", x=" + x +
+              ", erosionLevel=" + erosionLevel +
+              ", geologicIndex=" + geologicIndex +
+              ", regionType=" + regionType +
+              '}';
+    }
+
+    int x;
+    int erosionLevel;
+    long geologicIndex;
+    RegionType regionType;
+
+    void calculateGeologicIndex(List<Region> regions) {
+      if (this.y == 0 && this.x == 0) {
+        this.geologicIndex = 0;
+      } else if (this.y == TARGETY && this.x == TARGETX) {
+        this.geologicIndex = 0;
+      } else if (this.y == 0) {
+        this.geologicIndex = this.x * 16807;
+      } else if (this.x == 0) {
+        this.geologicIndex = this.y * 48271;
+      } else {
+        Region region1 =
+            regions.stream().filter(r -> r.y == y && r.x == x - 1).findFirst().orElseThrow();
+        Region region2 =
+            regions.stream().filter(r -> r.y == y - 1 && r.x == x).findFirst().orElseThrow();
+        this.geologicIndex = region1.erosionLevel * region2.erosionLevel;
+      }
+    }
+
+    int calculateErosionLevel(int y, int x) {
+      this.erosionLevel = (int) ((this.geologicIndex + DEPTH) % 20183);
+      return this.erosionLevel;
+    }
+
+    void setRegionType() {
+      switch (this.erosionLevel % 3) {
+        case 0:
+          this.regionType = RegionType.ROCKY;
+          break;
+        case 1:
+          this.regionType = RegionType.WET;
+          break;
+        case 2:
+          this.regionType = RegionType.NARROW;
+          break;
+        default:
+          throw new IllegalArgumentException();
+      }
+    }
+  }
+
+  enum RegionType {
+    ROCKY,
+    WET,
+    NARROW
+  }
+}
---
+++ day22/src/test/java/Day22Test.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day22Test {
+
+  @Test
+  public void testDay6Result1() {
+    try {
+      Day22 day22 = new Day22();
+      assertEquals(-1, day22.getResult1());
+    } catch (Exception e) {
+      assert (false);
+    }
+  }
+
+  @Test
+  public void testDay6Result2() {
+    try {
+      Day22 day22 = new Day22();
+      assertEquals(-1, day22.getResult2());
+    } catch (Exception e) {
+      assert (false);
+    }
+  }
+}
