069306bdd7eb655851ab0acd978c7c402cbcafd2
stanciua <andrei.stanciu@aciworldwide.com>
2019-08-02 17:01:29 +0300

    Ongoing work for part 1

--- day17/src/main/java/Day17.java
+++ day17/src/main/java/Day17.java
@@ -8,9 +8,12 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
+import org.javatuples.Triplet;
 
 class Day17 {
   char[][] slice2D;
+  final int maxX;
+  final int maxY;
   private static final Pattern veinsOfClay =
       Pattern.compile("(x|y)=(\\d+),\\s(x|y)=(\\d+)\\.\\.(\\d+)");
 
@@ -51,12 +54,12 @@ class Day17 {
         }
       }
     }
-    int maxY =
+    maxY =
         clayLocations.stream()
             .map(p -> p.getValue0())
             .max(Comparator.comparing(Integer::intValue))
             .orElseThrow();
-    int maxX =
+    maxX =
         clayLocations.stream()
             .map(p -> p.getValue1())
             .max(Comparator.comparing(Integer::intValue))
@@ -75,7 +78,62 @@ class Day17 {
     // initialize the clay cells with '#'
     clayLocations.stream().forEach(c -> slice2D[c.getValue0()][c.getValue1()] = '#');
 
-    //    displaySlice2D();
+    displaySlice2D();
+  }
+
+  void fillWater(char[][] slice2D, int row, int col) {
+    while (true) {
+      if (row > maxY) {
+        return;
+      }
+
+      if (slice2D[row][col] == '#' || slice2D[row][col] == '~') {
+        // can it hold water?
+        var holdingWater = canHoldWater(slice2D, row, col);
+        if (holdingWater.getValue0()) {
+          int leftClay = holdingWater.getValue1();
+          int rightClay = holdingWater.getValue2();
+          final int rowFinal = row;
+          IntStream.range(leftClay + 1, rightClay).forEach(i -> slice2D[rowFinal - 1][i] = '~');
+          row -= 2;
+        } else {
+          // if we cannot hold water we need to see in which direction the water will flow:
+          //  - left = 1
+          //  - right = 2
+          //  - left and right = 3
+          int direction = flowOfWaterDirection(slice2D, row, col);
+        }
+      }
+    }
+  }
+  int flowOfWaterDirection(char[][] slice2D, int row, int col) {
+    return -1;
+  }
+  Triplet<Boolean, Integer, Integer> canHoldWater(char[][] slice2D, int x, int y) {
+    int firstLeftClayPosition = -1;
+    int firstRightClayPosition = -1;
+
+    for (int i = x + 1; i <= maxX; i++) {
+      if (slice2D[y - 1][i] == '#') {
+        firstRightClayPosition = i;
+        break;
+      }
+    }
+
+    for (int i = x - 1; i <= 0; i++) {
+      if (slice2D[y - 1][i] == '#') {
+        firstLeftClayPosition = i; break; } }
+
+    if (firstLeftClayPosition == -1 || firstRightClayPosition == -1) {
+      return new Triplet<>(false, -1, -1);
+    }
+
+    // now we need to make sure that we have a bottom for the water to sit on
+    return new Triplet<>(
+        IntStream.rangeClosed(firstLeftClayPosition, firstRightClayPosition)
+            .allMatch(idx -> slice2D[y][idx] == '#' || slice2D[y][idx] == '~'),
+        firstLeftClayPosition,
+        firstRightClayPosition);
   }
 
   void displaySlice2D() {
@@ -87,220 +145,11 @@ class Day17 {
     }
   }
 
-  void getClayCorners(
-      List<Pair<Integer, Integer>> leftCorners, List<Pair<Integer, Integer>> rightCorners) {
-    int height = slice2D.length;
-    int width = slice2D[0].length;
-
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        char square = slice2D[i][j];
-        if (square == '#'
-            && i - 1 >= 0
-            && j + 1 < width
-            && slice2D[i - 1][j] == '#'
-            && slice2D[i][j + 1] == '#') {
-          leftCorners.add(new Pair<>(i, j));
-        }
-        if (square == '#'
-            && i - 1 >= 0
-            && j - 1 < width
-            && slice2D[i - 1][j] == '#'
-            && slice2D[i][j - 1] == '#') {
-          rightCorners.add(new Pair<>(i, j));
-        }
-      }
-    }
-  }
-
-  void buildClayList(
-      List<Clay> clays,
-      List<Pair<Integer, Integer>> leftCorners,
-      List<Pair<Integer, Integer>> rightCorners) {
-    for (var leftCorner : leftCorners) {
-      for (var rightCorner : rightCorners) {
-        int y3 = leftCorner.getValue0();
-        int x3 = leftCorner.getValue1();
-        int y4 = rightCorner.getValue0();
-        int x4 = rightCorner.getValue1();
-        int x1 = x3;
-        int y1 = y3;
-        int x2 = x4;
-        int y2 = y4;
-        // if this is a bottom
-        if (y3 == y4 && IntStream.rangeClosed(x3, x4).allMatch(x -> slice2D[y3][x] == '#')) {
-          for (int i = y3; i >= 0; i--) {
-            if (slice2D[i][x3] != '#') {
-              y1 = i + 1;
-              break;
-            }
-          }
-          for (int i = y4; i >= 0; i--) {
-            if (slice2D[i][x4] != '#') {
-              y2 = i + 1;
-              break;
-            }
-          }
-
-          clays.add(
-              new Clay(
-                  new Pair<>(y1, x1), new Pair<>(y2, x2), new Pair<>(y3, x3), new Pair<>(y4, x4)));
-        }
-      }
-    }
-  }
-
-  boolean didWaterReachBottom(Pair<Integer, Integer> waterPosition, List<Clay> clayList) {
-    for (Clay clay : clayList) {
-      int y3 = clay.getDownLeft().getValue0();
-      int x3 = clay.getDownLeft().getValue1();
-      int y4 = clay.getDownRight().getValue0();
-      int x4 = clay.getDownRight().getValue1();
-      int y = waterPosition.getValue0();
-      int x = waterPosition.getValue1();
-      
-      if (y == y3 - 1 && x > x3 && x < x4) {
-        // we have reached bottom of the clay
-        return true;
-      }
-    }
-    return false;
-  }
-
   int getResult1() {
-    List<Pair<Integer, Integer>> leftCorners = new ArrayList<>();
-    List<Pair<Integer, Integer>> rightCorners = new ArrayList<>();
-    getClayCorners(leftCorners, rightCorners);
-    List<Clay> clayList = new ArrayList<>();
-    buildClayList(clayList, leftCorners, rightCorners);
-    clayList.forEach(System.out::println);
-    List<Pair<Integer, Integer>> waterFlows = new ArrayList<>();
-    waterFlows.add(new Pair<>(0, 500));
     return -1;
-  }
-
-  static class Clay {
-
-    public Clay(
-        Pair<Integer, Integer> upperLeft,
-        Pair<Integer, Integer> upperRight,
-        Pair<Integer, Integer> downLeft,
-        Pair<Integer, Integer> downRight) {
-      this.upperLeft = upperLeft;
-      this.upperRight = upperRight;
-      this.downLeft = downLeft;
-      this.downRight = downRight;
-    }
-
-    @Override
-    public String toString() {
-      return "Clay{"
-          + "upperLeft="
-          + upperLeft
-          + ", upperRight="
-          + upperRight
-          + ", downLeft="
-          + downLeft
-          + ", downRight="
-          + downRight
-          + '}';
-    }
-
-    public Pair<Integer, Integer> getUpperLeft() {
-      return upperLeft;
-    }
-
-    public void setUpperLeft(Pair<Integer, Integer> upperLeft) {
-      this.upperLeft = upperLeft;
-    }
-
-    public Pair<Integer, Integer> getUpperRight() {
-      return upperRight;
-    }
-
-    public void setUpperRight(Pair<Integer, Integer> upperRight) {
-      this.upperRight = upperRight;
-    }
-
-    public Pair<Integer, Integer> getDownLeft() {
-      return downLeft;
-    }
-
-    public void setDownLeft(Pair<Integer, Integer> downLeft) {
-      this.downLeft = downLeft;
-    }
-
-    public Pair<Integer, Integer> getDownRight() {
-      return downRight;
-    }
-
-    public void setDownRight(Pair<Integer, Integer> downRight) {
-      this.downRight = downRight;
-    }
-
-    private Pair<Integer, Integer> upperLeft;
-    private Pair<Integer, Integer> upperRight;
-    private Pair<Integer, Integer> downLeft;
-    private Pair<Integer, Integer> downRight;
-  }
-
-  Square getNextSquareInWaterflow(Square lastSquare) {
-    return lastSquare;
   }
 
   int getResult2() {
     return -1;
-  }
-
-  static class Square {
-    public Square(Pair<Integer, Integer> position) {
-      this.position = position;
-    }
-
-    public Direction getDirection() {
-      return direction;
-    }
-
-    public void setDirection(Direction direction) {
-      this.direction = direction;
-    }
-
-    public Pair<Integer, Integer> getPosition() {
-      return position;
-    }
-
-    public void setPosition(Pair<Integer, Integer> position) {
-      this.position = position;
-    }
-
-    @Override
-    public String toString() {
-      return "Square{"
-          + "direction="
-          + direction
-          + ", position="
-          + position
-          + ", symbol="
-          + symbol
-          + '}';
-    }
-
-    public char getSymbol() {
-      return symbol;
-    }
-
-    public void setSymbol(char symbol) {
-      this.symbol = symbol;
-    }
-
-    Direction direction;
-    Pair<Integer, Integer> position;
-    char symbol;
-  }
-
-  enum Direction {
-    DOWN,
-    LEFT,
-    RIGHT
   }
 }
